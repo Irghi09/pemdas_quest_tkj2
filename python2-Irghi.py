e086b48164223c8817c7147d31009d208fb0319d
Irghi09 <111479772+Irghi09@users.noreply.github.com>
2022-08-18 13:55:57 +0700

    Add files via upload

---
+++ python2-Irghi.py
@@ -0,0 +1,7 @@
+nama_depan = "Irghi"
+print (nama_depan)
+print (" nama_depan anda adalah",nama_depan)
+
+nama_depan="Irghi"
+nama_belakang="Fahrezi"
+print ("nama_lengkap anda adalah",nama_depan,nama_belakang)
