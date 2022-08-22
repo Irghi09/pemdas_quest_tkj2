b544bd06cbe05bdd236617b29ff0309afa14bd83
Irghi09 <111479772+Irghi09@users.noreply.github.com>
2022-08-22 12:01:08 +0700

    Add files via upload

---
+++ python-latihan.py.py
@@ -0,0 +1,16 @@
+nd=("Nama depan ")
+nb=("Nama belakang ")
+print("nama saya adalah",nd,nb)
+
+
+a=int(input("masukan nilai a:"))
+b=float(input("masukan nilai b:"))
+c=a+b
+
+print("A=" ,a)
+print("B=" ,b)
+print("a,"+",b,"=",c)
+
+x=int(input("masukan suhu celcius: "))
+y=(9/5*x) + 32
+print(x,"celcius=" ,y, "fahrenheit")
