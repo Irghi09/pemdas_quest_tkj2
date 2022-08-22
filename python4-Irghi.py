851b0d442b4c16d1b714bf7d6544dce40fa679a4
Irghi09 <111479772+Irghi09@users.noreply.github.com>
2022-08-22 13:26:41 +0700

    Add files via upload

---
+++ python4-Irghi.py
@@ -0,0 +1,24 @@
+print("______________________________")
+print("Rumus Bangun Datar")
+print("______________________________")
+
+print("1. persegi\n")
+
+sisi = int(input("Masukkan Nilai Sisi: "))
+luas_p = sisi = sisi
+print("Luas Persegi adalah", luas_p)
+
+print("2. Persegi Panjang")
+
+panjang = int(input("Masukkan nilai panjang: "))
+lebar = int(input("Masukkan nilai lebar: "))
+luas_pp = panjang * lebar
+
+print("Luas persegi panjang adalah: ", luas_pp)
+
+print("3. Segitiga")
+
+alas = int(input("Masukkan nilai alas: "))
+tinggi = int(input("Masukkan nilai lebar: "))
+luas_s = alas = tinggi / 2 
+print("Luas segitiga adalah", luas_s)
